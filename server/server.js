e8f2f6b7faab59a9d276a9ae351f1696b7716a8b
arjun <vijayanarjun4@gmail.com>
2024-02-11 00:11:38 +0530

    Backend Connected to Database

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 require('dotenv').config()
 
-const app = require('express')();
+const {MongoClient} = require('mongodb')
 
+const app = require('express')();
+const mongourl = process.env.URI
 const PORT = 8080;
 
-app.get('/projects',(req,res)=>{
-    res.status(200).send({
-        'Status':'Success'
-    })
+const client = new MongoClient(mongourl)
+
+
+app.get('/projects',async(req,res)=>{
+    try{
+        await client.connect()
+        const data = await client.db('Portfolio').collection("Projects").findOne()
+        console.log(data)
+        res.status(200).send(data)
+    }catch(e){
+        console.log("Error")
+    }
 })
 
 app.listen(PORT,()=>{
