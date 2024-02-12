2ef0d16c6a72cc16f5ee7823f1f1c5b17c2e173c
arjun <vijayanarjun4@gmail.com>
2024-02-12 22:31:20 +0530

    Contact Page initial

--- client/src/Contact/Contact.jsx
+++ client/src/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-
+import './Contact.css'
 export default function Contact(){
     return(
         <div className="about" id="contact-navbar">
@@ -7,6 +7,9 @@ export default function Contact(){
             </div>
             <div className="about-right">
                <div className="contact-box">
+                <h2><a href='https://www.linkedin.com/in/vijayanarjun' target='blank'>LinkedIn</a></h2>
+                <h2><a href="https://github.com/notarjunvijayan" target='blank'>Github</a></h2>
+                <h2><a href="mailto:vijayanarjun4@gmail.com">Email</a></h2>
                </div>
             </div>
             
